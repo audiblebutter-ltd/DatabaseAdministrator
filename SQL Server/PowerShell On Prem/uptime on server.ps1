642600544e2621ce9b6b7ca5473a09dceb0f3f65
Winty <wintyboss@outlook.com>
2022-05-26 16:10:59 +0100

    Added server check script

---
+++ SQL Server/PowerShell On Prem/uptime on server.ps1
@@ -0,0 +1,10 @@
+﻿$ComputerName = $env:COMPUTERNAME
+$userSystem = Get-WmiObject win32_operatingsystem -ComputerName $ComputerName -ErrorAction SilentlyContinue
+if ($userSystem.LastBootUpTime) {
+$sysuptime= (Get-Date) - $userSystem.ConvertToDateTime($userSystem.LastBootUpTime)
+Write-Output ("Last boot: " + $userSystem.ConvertToDateTime($userSystem.LastBootUpTime) )
+Write-Output ("Uptime : " + $sysuptime.Days + " Days " + $sysuptime.Hours + " Hours " + $sysuptime.Minutes + " Minutes" )
+}
+else {
+Write-Warning "Unable to connect to $computername"
+}
