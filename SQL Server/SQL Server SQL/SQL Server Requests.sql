01b8795664f7ea7f80d472b233ab00fc0eebfbbe
Winty <wintyboss@outlook.com>
2022-11-15 15:04:05 +0000

    added grouping and sum

--- SQL Server/SQL Server SQL/SQL Server Requests.sql
+++ SQL Server/SQL Server SQL/SQL Server Requests.sql
@@ -1,1 +1,18 @@
 SELECT * FROM sys.dm_exec_requests;	
+
+SELECT 
+		count(1) numsess	, 
+		status	, 
+		command	, 
+		@@SERVERNAME insname 
+		FROM sys.dm_exec_requests
+		group by 
+				status ,
+				command
+		union
+		select
+				count(1) numsess	, 
+		'Total Sessions'	, 
+		'Total Sessions'	, 
+		'Total Sessions' insname 
+		FROM sys.dm_exec_requests
